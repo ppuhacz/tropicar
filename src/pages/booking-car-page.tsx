675c781bbb1053af91f520567a73cd46b15050cb
ppuhacz <ppuhacz@gmail.com>
2023-05-04 14:05:48 +0200

    Added RWD to contact form in car offers, improved accessibility, started working on booking page

--- src/pages/booking-car-page.tsx
+++ src/pages/booking-car-page.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import Layout from "../components/layout/layout";
-import CarBooking from "../components/car-booking/car-booking";
 
 const CarBookingPage = () => {
   return (
     <Layout>
-      {/* <CarBooking /> */}
       <h2>Booking (work in progress)</h2>
     </Layout>
   );
