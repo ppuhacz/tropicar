3ec72e9e8bed163fb4edbfa1d966dc3e87c27795
ppuhacz <ppuhacz@gmail.com>
2023-05-09 20:46:40 +0200

    Added car booking page, minor performance adjustments

--- src/pages/booking-car-page.tsx
+++ src/pages/booking-car-page.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import Layout from "../components/layout/layout";
+import CarBooking from "../components/car-booking/car-booking";
 
 const CarBookingPage = () => {
   return (
     <Layout>
-<<<<<<< HEAD
-      <h2>Booking (work in progress)</h2>
-=======
       <CarBooking />
->>>>>>> work-in-progress
     </Layout>
   );
 };
