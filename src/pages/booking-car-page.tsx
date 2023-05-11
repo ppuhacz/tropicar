de1848495d61eea73b69e9505df6ef0cc6bb035f
ppuhacz <ppuhacz@gmail.com>
2023-05-11 12:10:50 +0200

    Added booking form validation

--- src/pages/booking-car-page.tsx
+++ src/pages/booking-car-page.tsx
@@ -5,15 +5,7 @@ import CarBooking from "../components/car-booking/car-booking";
 const CarBookingPage = () => {
   return (
     <Layout>
-<<<<<<< HEAD
-<<<<<<< HEAD
-      <h2>Booking (work in progress)</h2>
-=======
       <CarBooking />
->>>>>>> work-in-progress
-=======
-      <CarBooking />
->>>>>>> 49e67c7b14168540d7cf51a1fcb540a20df50ef9
     </Layout>
   );
 };
